9b0958463242c418dbfd4a1edea331b87c4f85ee
john-hill <john.hill@sagebase.org>
2015-02-10 22:55:28 -0800

    phase

--- src/main/java/org/sagebionetworks/migrate/MigrateLocationables.java
+++ src/main/java/org/sagebionetworks/migrate/MigrateLocationables.java
@@ -2,11 +2,10 @@ package org.sagebionetworks.migrate;
 
 import java.io.File;
 import java.io.FileOutputStream;
+import java.io.IOException;
 import java.io.OutputStream;
 import java.io.OutputStreamWriter;
 import java.util.LinkedHashSet;
-import java.util.LinkedList;
-import java.util.List;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -19,6 +18,7 @@ import au.com.bytecode.opencsv.CSVWriter;
 
 public class MigrateLocationables {
 	
+	private static final int BATCH_SIZE = 1000;
 	private static final String NEW_TYPE = "new_type";
 	private static final String ERROR = "error";
 	private static final String CREATED_BY = "created_by";
@@ -50,8 +50,11 @@ public class MigrateLocationables {
 			CSVWriter writer = new CSVWriter(new OutputStreamWriter(fos, "UTF-8"));
 			writer.writeNext(RESULT_HEADER);
 			// Write each
+			int count = 1;
 			for(String id: locationables){
+				System.out.println("Migrating "+count+"/"+locationables.size());
 				translateLocation(id, writer);
+				count++;
 			}
 			writer.flush();
 		}finally{
@@ -64,8 +67,9 @@ public class MigrateLocationables {
 	 * Translate a single entity.
 	 * @param id
 	 * @param writer
+	 * @throws IOException 
 	 */
-	private void translateLocation(String id, CSVWriter writer){
+	private void translateLocation(String id, CSVWriter writer) throws IOException{
 		String[] results = new String[RESULT_HEADER.length];
 		set(results, ENTITY_ID, id);
 		try{
@@ -81,6 +85,7 @@ public class MigrateLocationables {
 			set(results, ERROR, e.getMessage());
 		}
 		writer.writeNext(results);
+		writer.flush();
 	}
 	
 	/**
@@ -112,16 +117,18 @@ public class MigrateLocationables {
 	 * @return
 	 * @throws JSONException
 	 * @throws SynapseException
+	 * @throws InterruptedException 
 	 */
-	private LinkedHashSet<String> getAllLocationableIds() throws JSONException, SynapseException {
+	private LinkedHashSet<String> getAllLocationableIds() throws JSONException, SynapseException, InterruptedException {
 		long totalNumber = 0;
-		long limit = 1000;
+		long limit = BATCH_SIZE;
 		long offset = 1;
 		LinkedHashSet<String> set = new LinkedHashSet<String>();
+		System.out.println("Gathering locationable ids...");
 		// First find all locationables
 		do{
 			String query = "select id from locationable limit "+limit+" offset "+offset;
-			JSONObject results = synapse.query(query);
+			JSONObject results = queryWithRetry(query);
 			totalNumber = results.getLong("totalNumberOfResults");
 			JSONArray array = results.getJSONArray("results");
 			for(int i=0; i<array.length(); i++){
@@ -131,9 +138,26 @@ public class MigrateLocationables {
 			}
 			// next page
 			offset = offset+limit;
+			System.out.println("Found "+set.size()+"/"+totalNumber);
 		}while(set.size() < totalNumber);
 		System.out.println("Found: "+set.size()+" Locationables");
 		return set;
+	}
+
+	public JSONObject queryWithRetry(String query) throws SynapseException, InterruptedException {
+		try {
+			return synapse.query(query);
+		} catch (Exception e) {
+			System.out.println("Will retry:"+e.getMessage());
+			Thread.sleep(10000);
+			try {
+				return synapse.query(query);
+			} catch (Exception e2) {
+				System.out.println("Will retry:"+e.getMessage());
+				Thread.sleep(10000);
+				return synapse.query(query);
+			}
+		}
 	}
 
 	/**
